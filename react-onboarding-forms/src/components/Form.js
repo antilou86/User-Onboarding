5671ff9fe48c7163f80e52c2c04ad3f03624fd06
Lou <antilou86@gmail.com>
2019-08-07 16:29:34 -0400

    WIP: built out basic form and values, working on makeFormik object/function/thing now

--- react-onboarding-forms/src/components/Form.js
+++ react-onboarding-forms/src/components/Form.js
@@ -10,6 +10,31 @@ const Form = () => {
 
     return (
         <div className='form-field'> 
+            <h1>Enter a New User</h1>
+            
+            <Form>
+
+                <Field type="text" name="name" placeholder="enter name..."/>
+                <Field type="email" name="email" placeholder="enter email..."/>
+                <Field type="password" name="password" placeholder="enter password..."/>
+                
+                <label>
+                    Check to agree to our Terms of Service
+                <Field type="checkbox" name="tos"/> 
+                </label>
+                
+                <Field component='select' name='role'>
+                    <option value='leader'>Leader</option>
+                    <option valuer='follower'>Follower</option>
+                    <option value='black-sheep'>Black Sheep</option>
+                </Field>
+                
+                <button type="submit">Submit!</button>
+            </Form>
+
+            {users.map(user => (
+                <></>
+            ))}
 
         </div>
     );
