9c83cee934b48cc639cbeac871a320d7ec26a5bf
Lou <antilou86@gmail.com>
2019-08-07 17:43:02 -0400

    MVP and Stretch Complete

--- react-onboarding-forms/src/components/Form.js
+++ react-onboarding-forms/src/components/Form.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import './form.css'
 
-const Form = ({errors, touched, values, handleSubmit, status}) => {
+const FormThang= ({errors, touched, values, handleSubmit, status}) => {
     
     const [users, setUsers] = useState([]);
     console.log(users);
@@ -15,18 +16,20 @@ const Form = ({errors, touched, values, handleSubmit, status}) => {
       }, [status]);
 
     return (
-        <div className='form-field'> 
+        <div className='form-div'> 
             <h1>Enter a New User</h1>
             
-            <Form>
-
-                <Field type="text" name="name" placeholder="enter name..."/>
+            <Form className="form-container">
+                
+                <Field className="text-field" type="text" name="name" placeholder="enter name..."/>
                 {touched.name && errors.name && (<p className="error">{errors.name}</p>)}
-
-                <Field type="email" name="email" placeholder="enter email..."/>
-                {touched.email && errors.email && (<p classNaemailme="error">{errors.email}</p>)}
-
-                <Field type="password" name="password" placeholder="enter password..."/>
+                
+               
+                <Field className="text-field" type="email" name="email" placeholder="enter email..."/>
+                {touched.email && errors.email && (<p className="error">{errors.email}</p>)}
+                
+                
+                <Field className="text-field" type="password" name="password" placeholder="enter password..."/>
                 {touched.password && errors.password && (<p className="error">{errors.password}</p>)}
 
                 <label>
@@ -34,8 +37,9 @@ const Form = ({errors, touched, values, handleSubmit, status}) => {
                     <Field type="checkbox" name="tos" checked={values.tos}/> 
                     <span className="checkbox-span"/>
                 </label>
+                {errors.tos && (<p className="error">{errors.tos}</p>)}
                 
-                <Field component='select' name='role'>
+                <Field component='select' name='role' className="text-field">
                     <option value='leader'>Leader</option>
                     <option valuer='follower'>Follower</option>
                     <option value='black-sheep'>Black Sheep</option>
@@ -43,11 +47,11 @@ const Form = ({errors, touched, values, handleSubmit, status}) => {
                 {touched.role && errors.role && (<p className="error">{errors.role}</p>)}
 
                 
-                <button type="submit">Submit!</button>
+                <button className="submit-button" type="submit">Submit!</button>
             </Form>
-
+            <h2>Current Users:</h2>
             {users.map(user => (
-                <p key={user.id}>{user.name} - {user.email}</p>
+                <p key={user.id}>{user.name} - {user.email} - {user.role}</p>
             ))}
 
         </div>
@@ -78,9 +82,9 @@ const FormikForm = withFormik({
         name: Yup.string().required('but who are you though?'),
         email: Yup.string().required('please provide your email'),
         password: Yup.string().min(8,'minimum of 8 characters required').required('your password protects you! dont forget to add one.'),
-        tos: Yup.boolean().required('Accept the ToS or forever be denied entry.')
+        tos: Yup.boolean(true).required('Accept the ToS or forever be denied entry.')
     })
 
-})(Form);
+})(FormThang);
 
 export default FormikForm
