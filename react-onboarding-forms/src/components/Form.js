64ea16050012c332d8d45b90719e27e4b5d2d62b
Lou <antilou86@gmail.com>
2019-08-07 16:13:55 -0400

    WIP: imported all dependencies, set up components folder, handled the majority of imports. building out formik form next.

---
+++ react-onboarding-forms/src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const Form = () => {
+
+    const [users, setUsers] = useState([]);
+    console.log(users);
+
+    return (
+        <div className='form-field'> 
+
+        </div>
+    );
+}
+
+
+export default FormikForm
